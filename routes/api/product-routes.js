76d618b5f5140e5b98f8e17c8fa90a3e8da9aadf
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-04-01 11:42:49 -0700

    define columns in models and started product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const oneProduct = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: tag }],
+    });
+
+    if (!driverData) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
